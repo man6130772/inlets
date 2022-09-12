1d58a0953a2a13e60937ea28584326c7c3334747
trusman_chen <357063127@qq.com>
2022-09-12 17:55:29 +0800

    Update client.go

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -11,7 +11,7 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/inlets/inlets/pkg/transport"
+	"github.com/man6130772/inlets/pkg/transport"
 	"github.com/rancher/remotedialer"
 	"github.com/twinj/uuid"
 )
